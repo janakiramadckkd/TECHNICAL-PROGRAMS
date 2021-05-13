303f6d4751be7f5e0003e418efe75b13f97d1dc6
janakiramadckkd <84022252+janakiramadckkd@users.noreply.github.com>
2021-05-13 19:27:43 +0530

    Add files via upload

---
+++ 5.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int i,j,sum,k,n,c=0;
+	    Scanner s=new Scanner(System.in);
+	    n=s.nextInt();
+	    int a[]=new int[n];
+	    for(i=0;i<n;i++)
+	    a[i]=s.nextInt();
+	   
+	    k=s.nextInt();
+	 for ( i = 0; i < n; i++) {
+             
+            sum = 0;
+            for ( j = i; j < n; j++) {
+                 
+                // calculate required sum
+                sum += a[j];
+                 
+                // check if sum is equal to
+                // required sum
+                if (sum % k==0)
+                    c++;
+            }
+        }
+        System.out.println(c);
+	}
+}
